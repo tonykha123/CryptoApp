caa3d7e0b109133b02e54f10f82977e50bbf30ce
Tony K <tonykha.1705@gmail.com>
2022-06-25 13:07:22 +1200

    setup

--- client/index.js
+++ client/index.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+// import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById('app')
-  )
+  ReactDOM.render(<App />, document.getElementById('app'))
 })
