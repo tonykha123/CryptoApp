f5be4d078377d8f22366b127b5f40d357755f558
Tony K <tonykha.1705@gmail.com>
2022-06-25 14:30:03 +1200

    fetch data from crypto

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,19 @@
-
 import React, { useEffect, useState } from 'react'
 import * as api from '../apis/apiFile'
 
-function App () {
-
-  useEffect (async ()=> {
-try {
-      const crypto= await api.getCrypto()
+function App() {
+  useEffect(async () => {
+    try {
+      const crypto = await api.getCrypto()
       console.log(crypto)
-      
-    } catch (error) { console.error}
-
-  },[])
-
+    } catch (error) {
+      console.error
+    }
+  }, [])
 
   return (
     <>
-   
-    <h1>heyyy</h1>
+      <h1>heyyy</h1>
     </>
   )
 }
