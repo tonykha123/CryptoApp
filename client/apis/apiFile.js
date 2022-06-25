e69b843dc75b2d360307567ae4f4b22533c61ec6
Tony K <tonykha.1705@gmail.com>
2022-06-25 14:00:24 +1200

    fetched data form crypto

--- client/apis/apiFile.js
+++ client/apis/apiFile.js
@@ -7,3 +7,13 @@ export function getFruits() {
     return res.body.fruits
   })
 }
+
+const getCrypto = async () => {
+  const res = await request.get(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=nzd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  )
+
+  return res.body
+}
+
+export { getCrypto }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react'
 
-import { getFruits } from '../apis/api'
+import React, { useEffect, useState } from 'react'
+import * as api from '../apis/apiFile'
 
 function App () {
+
+  useEffect (async ()=> {
+try {
+      const crypto= await api.getCrypto()
+      console.log(crypto)
+      
+    } catch (error) { console.error}
+
+  },[])
 
 
   return (
     <>
-     
+   
+    <h1>heyyy</h1>
     </>
   )
 }
